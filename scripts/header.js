51ffb8a5a40c9aea2551d98f49858d9596b531de
jnnieto <niconieto1506@gmail.com>
2020-12-22 21:24:54 -0500

    Programa con carpetas

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if($(this).scrollTop()  >  740){
+        if($(this).scrollTop()  >  600){
             $('header').addClass('header2');   
         }else{
             $('header').removeClass('header2'); 
